7e6dcc76a65874b63b6e0e251486a99c8f497b9a
TuanDo <92799704+paintOfUs@users.noreply.github.com>
2023-03-23 22:46:52 +0700

    Update createCSV.py

--- createCSV.py
+++ createCSV.py
@@ -5,6 +5,7 @@ import numpy as np
 def write_data(data):
 
   df = pd.DataFrame(data, columns=get_key(data))
+  # muốn hiển thị ra chữ trong csv thì chính lại thành utf16 nhưng khuyên ko nên vì ko cần thiết và nó khiến sai định dạng lưu(Không phải lỗi)
   df.to_csv("data.csv",encoding='utf8', index=False)
   d = pd.read_csv('data.csv')
   d.head()
