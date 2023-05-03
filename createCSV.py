300e8035aabcd6eee18b7d0606366c8364690b46
TuanDo <92799704+paintOfUs@users.noreply.github.com>
2023-05-03 09:21:13 +0700

    Update createCSV.py

--- createCSV.py
+++ createCSV.py
@@ -1,11 +1,10 @@
 import csv
 
 def write_data(data):
-  data = [[s.strim().replace('\n','') for s in sub_list] for sub_list in data]
+  data = [[s.strip().replace('\n','') for s in sub_list] for sub_list in data]
   with open('data.csv','w', encoding="utf-8-sig",newline='') as file:
    list_data = csv.writer(file,quoting=csv.QUOTE_MINIMAL )
    list_data.writerow(['Text','Evaluate'])
    for x in data:
       row = [cell.replace('"','') for cell in x]
       list_data.writerow(row)
-  
