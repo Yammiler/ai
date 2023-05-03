d4d86d53c20f6e7fe08ecbffffd136a79a0d2145
TuanDo <92799704+paintOfUs@users.noreply.github.com>
2023-05-03 12:20:25 +0700

    Update createCSV.py

--- createCSV.py
+++ createCSV.py
@@ -8,3 +8,12 @@ def write_data(data):
    for x in data:
       row = [cell.replace('"','') for cell in x]
       list_data.writerow(row)
+      
+def add_data(data):
+  data = [[s.strip().replace('\n','') for s in sub_list] for sub_list in data]
+  with open('data.csv','a', encoding="utf-8-sig",newline='') as file:
+   list_data = csv.writer(file,quoting=csv.QUOTE_MINIMAL )
+   for x in data:
+      row = [cell.replace('"','') for cell in x]
+      list_data.writerow(row)
+  
